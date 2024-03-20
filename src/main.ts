af75975c63758e18289d7a39eea9ec4c18551776
Sai Koushik <koushik.vanama@gmail.com>
2024-03-21 00:09:52 +0530

    chore: counter logic, tests, eslint, prettier

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,35 @@
-import { factory } from "./factory";
+import { factory } from './factory';
 
 let count = factory(0, 1);
 
-function update_count_and_reset_counter() {}
+function update_count_and_reset_counter() {
+  count = factory(Number(start_at_control.value), Number(step_control.value));
+  update_count();
+}
 
 const start_at_control = document.getElementById(
-  "start_at",
+  'start_at',
 ) as HTMLInputElement;
 
-const step_control = document.getElementById("step") as HTMLInputElement;
+const step_control = document.getElementById('step') as HTMLInputElement;
 
-start_at_control?.addEventListener("change", () => {});
+start_at_control?.addEventListener('change', update_count_and_reset_counter);
 
-step_control?.addEventListener("change", () => {});
+step_control?.addEventListener('change', update_count_and_reset_counter);
 
 const count_button = document.querySelector(
-  ".count_button",
+  '.count_button',
 ) as HTMLButtonElement;
 
 const current_count = document.querySelector(
-  ".current_count",
+  '.current_count',
 ) as HTMLSpanElement;
 
-function update_count() {}
+function update_count() {
+  const currentCount = count();
+  current_count.innerText = currentCount.toString();
+}
 
-count_button.addEventListener("click", update_count);
+update_count();
+
+count_button.addEventListener('click', update_count);
