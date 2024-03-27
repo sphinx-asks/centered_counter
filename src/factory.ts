1e51f9073f3e3f32d8fbd3ff757b5b536ec9353f
shirisha-sakinala <sakinalashirisha@gmail.com>
2024-03-27 22:26:08 +0530

    task changes

--- src/factory.ts
+++ src/factory.ts
@@ -5,4 +5,22 @@
  * @param {number} [step] - The step to increment each number in the sequence. Default is 1.
  * @returns {Function} - A function that generates the number sequence every time it is called.
  */
-export const factory = (start?: number, step?: number) => {};
+export const factory = (start?: number, step?: number): (() => number) => {
+    var current = start;
+    if(Number.isNaN(start) && Number.isNaN(step)) {
+        current = 0;
+        step = 1;
+    } else if (Number.isNaN(start)) {
+        current = 0;
+        step = step;
+    } else if (Number.isNaN(step)){
+        current = start;
+        step = 1;
+    }
+    return () => {
+        const value = current;
+        current += step;
+        return value;
+    };
+};
+
